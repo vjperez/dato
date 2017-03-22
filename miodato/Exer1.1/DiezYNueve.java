0d4dc65ce812e17c4419b2b131a3f4dbb139a30e
victor j. perez <victor.perez1@upr.edu>
2017-03-22 19:55:03 -0400

    static functions to calculate products, powers and fibonacci

---
+++ miodato/Exer1.1/DiezYNueve.java
@@ -0,0 +1,29 @@
+/*
+ *  Working with Fibonacci
+ */
+
+import edu.princeton.cs.algs4.StdOut;
+
+class DiezYNueve{
+    
+    public static long F(int f){
+        if(f == 0) return 0;
+        if(f == 1) return 1;
+        return F(f-2) + F(f-1);
+    }
+    
+
+    
+    
+    public static void main(String[] args){
+        StdOut.println("Fibonacci 0 = " + F(0));
+        StdOut.println("Fibonacci 1 = " + F(1));
+        StdOut.println("Fibonacci 2 = " + F(2));
+        StdOut.println("Fibonacci 3 = " + F(3));
+        StdOut.println("Fibonacci 4 = " + F(4));
+        StdOut.println("Fibonacci 5 = " + F(5));
+        StdOut.println("Fibonacci 6 = " + F(6));
+        StdOut.println("Fibonacci 7 = " + F(7));
+        StdOut.println("Fibonacci 8 = " + F(8));
+    }
+}
--- miodato/Exer1.1/DiezYOcho.java
+++ miodato/Exer1.1/DiezYOcho.java
@@ -2,7 +2,6 @@ import edu.princeton.cs.algs4.StdOut;
 
 class DiezYOcho{
     
-    
     public static int producto(int x, int y){
         if(y == 0) return 0;
         if(y % 2 == 0) return producto(x+x, y/2); // si "y" es par
@@ -10,9 +9,20 @@ class DiezYOcho{
     }
     
     
+    public static long power(int x, int y){
+        if(y==0) return 1;
+        if(y % 2 == 0) return power(x*x, y/2);
+        return x * power(x*x, y/2);
+    }
+    
+    
     public static void main(String[] args){
         StdOut.println("4 X 10 = " + producto(4,10));
         StdOut.println("12 X 100 = " + producto(12,100));
         StdOut.println("34 X 7 = " + producto(34,7));
+        
+        StdOut.println("2 ^ 10 = " + power(2,10));
+        StdOut.println("3 ^ 4 = " + power(3,4));
+        StdOut.println("5 ^ 4 = " + power(5,4));
     }
 }
