63b81e3c275a43a5ca4d26efb89003f4a3b96f7d
victor j. perez <victor.perez1@upr.edu>
2017-03-23 22:37:33 -0400

    adding a loop to calculate Fibonacci to a range of integers

--- miodato/Exer1.1/FibonacciCalculationTime.java
+++ miodato/Exer1.1/FibonacciCalculationTime.java
@@ -1,5 +1,7 @@
 /*
  *  Working with the calculation time of the Fibonacci series
+ *  Calculates fibonacci numbers from "a" to "b"
+ *  "a" and "b" are received as command line arguments
  */
 
 import edu.princeton.cs.algs4.StdOut;
@@ -17,15 +19,16 @@ class FibonacciCalculationTime{
     
     public static void main(String[] args){
         TimeInterval fibonacciCalculationTime = new TimeInterval();
-        int f = Integer.parseInt(args[0]);
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
         
-        fibonacciCalculationTime.setTimeA();
-        long result = F(f);
-        fibonacciCalculationTime.setTimeB();
+        for(int f = a; f < b; f++){
+            fibonacciCalculationTime.setTimeA();
+            long result = F(f);
+            fibonacciCalculationTime.setTimeB();
         
-        
-        //StdOut.printf("%.6f\n", 1.0/7.0);
-        StdOut.printf("Fibonacci " + args[0] + " = " + "%,d\n",result );
-        StdOut.println("Calculation took " + fibonacciCalculationTime.getMilliSecondsDiference() + " milliseconds.");
+            StdOut.printf("Fibonacci " + f + " = " + "%,d\n",result );
+            StdOut.println("Calculation took " + fibonacciCalculationTime + "\n");
+        }
     }
 }
