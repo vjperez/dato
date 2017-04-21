16d319b33a7c1cf32357392f9fab6850207e143a
victor j. perez <victor.perez1@upr.edu>
2017-04-20 22:11:25 -0400

    Test when 2 strings are circular rotations of each other.

--- miodato/one02/StringCircularRotation.java
+++ miodato/one02/StringCircularRotation.java
@@ -5,9 +5,10 @@
 public class StringCircularRotation{
     private StringCircularRotation(){}
     
-    public static boolean test(String a, String b){
+    public static boolean test(String a, String b, Counter c){
         int index = a.indexOf(b.substring(0,1), 0);
         while(index > -1 && index < a.length()){
+            c.increase();
 			if( b.equals(a.substring(index, a.length()) + a.substring(0, index)) ){
 				return true;
 			}else{
--- miodato/one02/StringCircularRotationTest.java
+++ miodato/one02/StringCircularRotationTest.java
@@ -10,10 +10,15 @@ public class StringCircularRotationTest{
     public static void main(String[] args){
         String a = args[0];
         String b = args[1];
+        
+        Counter indexes = new Counter("Amount of indexes searched");
    
-		if(StringCircularRotation.test(a, b))
-			StdOut.println(a + " and " + b + " are a circular rotation of each other.");	
-		else
+		if(StringCircularRotation.test(a, b, indexes)){
+			StdOut.println(a + " and " + b + " are a circular rotation of each other.");
+            StdOut.println(indexes);	
+        }else{
 			StdOut.println(a + " and " + b + " are NOT a circular rotation of each other.");
-	}
+            StdOut.println(indexes);
+        }
+    }
 }
