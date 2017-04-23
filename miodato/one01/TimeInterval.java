f88fa9fab577e2cdde6c40d059e6e232cf2af152
victor j. perez <vijapefi@gmail.com>
2017-04-23 10:31:31 -0400

    Separating time interval on package one01 from time interval on package one02

--- miodato/one01/TimeInterval.java
+++ miodato/one01/TimeInterval.java
@@ -1,41 +1,34 @@
 package one01;
 
 import java.util.Date;
-import java.time.Instant;
-import java.time.Duration;
 
+/**
+ * Measuring a time interval using java.util.Date
+ */
 public class TimeInterval{
     private long timeA;
     private long timeB;
-    
-    private Instant start;
-    private Instant stop;    
+   
     
     public TimeInterval(){
         Date now = new Date();
         timeA = timeB = now.getTime();
-        start = stop = Instant.now();
     }
 
     public void setTimeA(){
         Date now = new Date();
         timeA = now.getTime();
-        start = Instant.now();
     }
     
     public void setTimeB(){
         Date now = new Date();
         timeB = now.getTime();
-        stop = Instant.now();
     }
     
     public long getMilliSecondsDiference(){
         return timeB-timeA;
     }
-    public Duration getDuration(){
-		return Duration.between(start, stop);
-	}
-    
+        
     public String toString(){
         long longDiff = getMilliSecondsDiference();
         long hours = longDiff / 3600000;
--- miodato/one01/TimeIntervalTest.java
+++ miodato/one01/TimeIntervalTest.java
@@ -7,11 +7,11 @@ class TimeIntervalTest{
     public static void main(String[] args){
         TimeInterval intervalo = new TimeInterval();
         StdOut.println("Initial java.util.date based diff : " + intervalo.getMilliSecondsDiference() + " milliseconds.");
-        StdOut.println("Initial Duration, java.time.Duration based: " + intervalo.getDuration());
+        
         
         intervalo.setTimeB();
         StdOut.println("\nAfter 2 print instructions, java.util.date based diff: " + intervalo.getMilliSecondsDiference() + " milliseconds.");
-        StdOut.println("Duration after 2 print instructions, java.time.Duration based: " + intervalo.getDuration() );
+        
         StdOut.println("");
         for(byte b = 0; b < 100; b++) { // using 128 or bigger, you get an overflow and infinite loop 
                 StdOut.print(b + " : ");
@@ -21,12 +21,12 @@ class TimeIntervalTest{
         
         intervalo.setTimeB();
         StdOut.println("After 105 print instructions, java.util.date based diff: " + intervalo.getMilliSecondsDiference() + " milliseconds.");
-        StdOut.println("Duration after 105 print instructions, java.time.Duration based: " + intervalo.getDuration());
+        
         
         StdOut.println("\nType a number :"); 
         StdIn.readInt();
         intervalo.setTimeB();
         StdOut.println("\nAfter input, java.util.date based diff: " + intervalo); // uses toString()
-        StdOut.println("Duration after input, java.time.Duration based: " + intervalo.getDuration());
+        
     }
 }
