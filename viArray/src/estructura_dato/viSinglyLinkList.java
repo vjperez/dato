1ce8ae189031826b92655d803862b05929fc1d11
victor j. perez <vijapefi@gmail.com>
2024-04-26 09:00:57 -0400

    implementing a singly link list

--- viArray/src/estructura_dato/viSinglyLinkList.java
+++ viArray/src/estructura_dato/viSinglyLinkList.java
@@ -17,15 +17,84 @@ public class viSinglyLinkList<E> implements viList<E>{
         return this.size;
     }
     
+    public boolean isEmpty(){ return  this.size == 0  &&  this.head == null; }
+
+    public void add(E elm){
+        this.add(this.size, elm);
+    }
+
+    public void add(int index, E elm){
+        if(index < 0 || index > this.size) throw new IndexOutOfBoundsException();
+        Nodo<E> toAdd = new Nodo<E> (elm);
+        if(index == 0){
+            toAdd.setNext( this.head );
+            this.head = toAdd;
+            this.size++;
+        }else{
+            Nodo<E> previous = this.getNode(-1 + index);
+            toAdd.setNext( previous.getNext() );
+            previous.setNext( toAdd );
+            this.size++;
+        }
+    }    
+
+    //toString() from Object class 
+    public String toString(){
+        String str = "[ ";
+        for(int n = 0; n < this.size(); n++){
+            if ((n + 1) == this.size())           str += this.get(n)       ;
+            else                                  str += this.get(n) + ", ";
+        }
+        str += " ]";
+        return str;
+    }
+
+    public E get(int index){
+        if(index < 0 || index >= this.size) throw new IndexOutOfBoundsException();
+        return this.getNode(index).getElemento();
+    }
+
+    public Nodo<E> getNode(int index){
+        if(index < 0 || index >= this.size) throw new IndexOutOfBoundsException();
+        Nodo<E> currentNodo = this.head;
+        for(int n = 0; n < index; n++){
+            currentNodo = currentNodo.getNext();
+        }
+        return currentNodo;
+    }  
 
     private class Nodo<E>{
         private E elemento;
         private Nodo<E> next; 
 
         public Nodo(){
+            this(null, null);
+        }
+        public Nodo(E elm){
+            this(null, elm);
+        }
+        public Nodo(Nodo<E> next, E elm){
+            this.elemento = elm;
+            this.next = next;
+        }        
+
+
+        public void clear(){ 
             this.elemento = null;
             this.next = null;
         }
-    }
+        public Nodo<E> getNext(){
+            return this.next;
+        } 
+        public E getElemento(){
+            return this.elemento;
+        }  
+        public void setNext(Nodo<E> next){
+            this.next = next;
+        } 
+        public void setElemento(E elm){
+            this.elemento = elm;
+        }                 
+    }//Nodo
 
 }//viSinglyLinkList
--- viArray/src/main/viSinglyLinkListTest.java
+++ viArray/src/main/viSinglyLinkListTest.java
@@ -5,20 +5,25 @@ import interfases.viList;
 
 public class viSinglyLinkListTest{
     public static void main(String[] args){
-        viList <String> lista = new viSinglyLinkList <String>(1);
- 
+        viList <String> lista = new viSinglyLinkList<String> ();
+        System.out.println( lista );
+        lista.add("victor");
+        System.out.println( lista );
 
-        viList <Integer> listaInt = new viSinglyLinkList <Integer>();
-     
+        viList <Integer> listaInt = new viSinglyLinkList<Integer> ();
+        System.out.println( listaInt );
+        listaInt.add(1024);
+        System.out.println( listaInt );
+
+
         //print with Iterator
-        
     }
 
     public static void toUpper( viList<String> list){
         for(int n = 0; n < list.size(); n++){
             String firstLetterToUpper = String.valueOf( list.get(n).charAt(0) ).toUpperCase();
             String otherLetters = list.get(n).substring(1);
-            list.set( n,  firstLetterToUpper + otherLetters  ) ;
+            //list.set( n,  firstLetterToUpper + otherLetters  ) ;
         }
     }
 }
