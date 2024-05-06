5768a9368ff1bcbd99a1c472b5f461a72eb65a13
victor j. perez <vijapefi@gmail.com>
2024-05-06 14:16:26 -0400

    rename

---
+++ viCollection/src/interfases/viMap.java
@@ -0,0 +1,18 @@
+package interfases;
+
+public interface viMap<K, V>{
+
+    public boolean isEmpty();
+    
+    public V put(K key, V value);
+
+    public V remove(K key);
+    
+    public V get(K key);
+
+    public int size();
+
+    public void clear();
+    
+    public boolean contains(K key);
+}
